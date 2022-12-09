00df45ff10a7f64a0a8bcc70c752e240e31a5ce5
SoranaGalan <65121404+SoranaGalan@users.noreply.github.com>
2022-12-09 13:55:37 +0200

    Update main.java

--- main.java
+++ main.java
@@ -8,10 +8,9 @@ public class main {
     public static void main(String[] args) throws Exception
     {
         List<List<String>> listOfLines = new ArrayList<>();
-
-        File file = new File("C:\\Users\\soran\\OneDrive\\Desktop\\PLAYGROUND\\lab3_finite_auto\\file.cvs");    //creates a new file instance
-        FileReader fileread = new FileReader(file);   //reads the file
-        BufferedReader br = new BufferedReader(fileread);  //creates a buffering character input stream
+        File file = new File("C:\\Users\\soran\\OneDrive\\Desktop\\PLAYGROUND\\lab3_finite_auto\\file.cvs");  
+        FileReader fileread = new FileReader(file);  
+        BufferedReader br = new BufferedReader(fileread);  
         String line;
         while((line=br.readLine())!=null) {
             String[] splited = line.split(",");
@@ -22,29 +21,21 @@ public class main {
             listOfLines.add(eachLine);
 
         }
-        //changes
         String initialStates = "initial states: ";
-        //
         String finalStates = "Final states: ";
-
         List<String> statesList = new ArrayList<>();
         List<String> alphabetList = new ArrayList<>();
 
         System.out.println("Transitions : ");
 
-
         for (List<String> elem : listOfLines){
-                //final states
                 for(String s : elem) {
                     if (s.equals("final"))
                         finalStates = finalStates + " " + elem.get(0);
-                    //
                     if (s.equals("initial"))
                         initialStates = initialStates + " " + elem.get(0);
-                    //
                 }
 
-                //states
                 if(!statesList.contains(elem.get(0))){
                     statesList.add(elem.get(0));
                 }
@@ -52,38 +43,28 @@ public class main {
                     statesList.add(elem.get(elem.size()-1));
                 }
 
-                //alphabet
                 if(!alphabetList.contains(elem.get(1)) && elem.size()>=3){
                     alphabetList.add(elem.get(1));
                 }
 
-                //transitions
             if(elem.size()>=3){
                     System.out.println(" ( " + elem.get(0) + " , "+ elem.get(1) + " ) " + " = " + elem.get(2));
                 }
         }
 
         System.out.println();
-
         String states = "States: ";
         for(String i : statesList)
             states = states + " " + i;
         System.out.println(states);
-
         System.out.println();
-        //
         System.out.println(initialStates);
-        System.out.println();
-        //
+        System.out.println();      
         System.out.println(finalStates);
-
         System.out.println();
-
         String alphabet = "Alphabet: ";
         for (String h : alphabetList)
             alphabet = alphabet+" "+h;
         System.out.println(alphabet);
-
     }
-
 }
